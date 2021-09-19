6676242191d9f8df09a46a1909d5c44c3569a6dd
Yukesh-21 <77238012+Yukesh-21@users.noreply.github.com>
2021-09-19 23:38:00 +0530

    Create Simple ex of object and class.java

---
+++ Simple ex of object and class.java
@@ -0,0 +1,10 @@
+class student1 {
+	int num;
+	String name;
+public static void main(String args[]) {
+	student1 s1 = new student1();
+	System.out.println(s1.num);
+	System.out.println(s1.name);
+	
+}
+}
